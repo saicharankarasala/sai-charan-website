d36eed57d4120217897f16283f5c25c44842ad45
KVSC1511 <kvsc1511@gmail.com>
2025-07-21 08:05:17 -0400

    Fix AnimatePresence warning: remove mode='wait' for project grid animations (safe for multi-child)

--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -58,7 +58,7 @@ const ProjectShowcase = ({ projects }) => {
         layout
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
       >
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           {filteredProjects.map((project, index) => (
             <motion.div
               key={project.title}
